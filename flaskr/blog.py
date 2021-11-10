c43453f64e421829e47e961ede9f9cec1d89677d
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-11-10 22:06:14 +0100

    🚧 adding route

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,18 +1,26 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
-
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint("blog", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username '
-        'FROM post p JOIN user u ON p.author_id = u.id '
-        'ORDER BY created DESC'
+        "SELECT p.id, title, body, created, author_id, username "
+        "FROM post p JOIN user u ON p.author_id = u.id "
+        "ORDER BY created DESC"
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
-    
+    return render_template("blog/index.html", posts=posts)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
